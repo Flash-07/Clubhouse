cf7d9d1e944d86ef1ad1b6a6eb25ab99b2c4e465
Abdulbaqui Ansari <abdulbaquia@neelafilm.com>
2025-02-11 11:37:42 +0530

    Update TMKOC.sol

--- contracts/TMKOC.sol
+++ contracts/TMKOC.sol
@@ -1,14 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
-contract TaarakMehtaKaOoltahChashmash is ERC20 {
+contract TaarakMehtaKaOoltahChashmash is ERC20, ERC20Permit {
     constructor(
         string memory name,
         string memory symbol,
         uint256 totalSupply_
-    ) ERC20(name, symbol) {
+    ) ERC20(name, symbol) ERC20Permit(name) {
         _mint(msg.sender, totalSupply_);
     }
 }
