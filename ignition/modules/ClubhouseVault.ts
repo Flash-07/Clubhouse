04e7d62b88cb0b2a86043a930a6ad39e2ef3bb33
Abdulbaqui Ansari <abdulbaquia@neelafilm.com>
2025-02-13 14:12:06 +0530

    Added new Contract and Scripts

--- ignition/modules/ClubhouseVault.ts
+++ ignition/modules/ClubhouseVault.ts
@@ -4,7 +4,7 @@ const ClubhouseVaultModule = buildModule("ClubhouseVaultModule", (m) => {
 
     // Specify the contract to deploy
     const clubhouseMain = m.contract("ClubhouseVault", [
-        "0x369baC795Dd369c6fDE5271A3835afE91536E0EA", // Address of TMKOC Token
+        "0x9ffd281286450cF2b7e20dC057b04cb7eB18D604", // Address of TMKOC Token
     ]);
 
     return { clubhouseMain };
---
+++ ignition/modules/Deploy.ts
@@ -0,0 +1,17 @@
+import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+
+const TMKOCAndClubhouseModule = buildModule("TMKOCAndClubhouseModule", (m) => {
+  // Deploy the TMKOC token contract first
+  const tmkoc = m.contract("TaarakMehtaKaOoltahChashmash", [
+    "Taarak Mehta Ka Ooltah Chashmah", // Token Name
+    "TMKOC",                           // Token Symbol
+    "100000000000000000000000000",        // Total Supply (100 million tokens with 18 decimals)
+  ]);
+
+  // Deploy the ClubhouseMain contract with the TMKOC contract address
+  const clubhouseMain = m.contract("ClubhouseMain", [tmkoc]);
+
+  return { tmkoc, clubhouseMain };
+});
+
+export default TMKOCAndClubhouseModule;
