144115c20ec4eb4f8019963efd3881738ab4bcdc
Abdulbaqui07 <abdulbaquia@neelafilm.com>
2025-02-24 17:48:04 +0530

    Updated  hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -13,7 +13,7 @@ const config: HardhatUserConfig = {
     localhost: {
       url: "http://127.0.0.1:8545"
     },
-    hardhat: {      
+    hardhat: {
     },
     mainnet: {
       url: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
@@ -42,7 +42,7 @@ const config: HardhatUserConfig = {
       accounts: [process.env.WALLET_PRIVATE_KEY || ""],
     },
     sepolia: {
-      url:`https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      url: `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`,
       accounts: {
         mnemonic: "test test test test test test test test test test test junk",
         path: "m/44'/60'/0'/0",
@@ -63,6 +63,10 @@ const config: HardhatUserConfig = {
       verifyURL: "https://explorer.sepolia.era.zksync.dev/contract_verification",
       // accounts: [process.env.ZKSYNC_PRIVATE_KEY as string],
     },
+    somnia: {
+      url: "https://dream-rpc.somnia.network",
+      accounts: [process.env.WALLET_PRIVATE_KEY || ""], // put dev menomonic or PK here,
+    },
   },
 
   etherscan: {
@@ -77,7 +81,7 @@ const config: HardhatUserConfig = {
   sourcify: {
     // Disabled by default
     // Doesn't need an API key
-    enabled:true
+    enabled: true
   },
 
   solidity: {
@@ -104,7 +108,7 @@ const config: HardhatUserConfig = {
             "devdoc"
           ],
         },
-      },     
+      },
     },
   },
 
